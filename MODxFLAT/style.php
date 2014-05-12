7479030ea0dc9d6b00453c6443d5529d111911c1
Nicola1971 <modxit@modx.it>
2014-05-12 18:05:07 +0200

    RC3 update

--- MODxFLAT/style.php
+++ MODxFLAT/style.php
@@ -11,7 +11,6 @@
 $style_path = 'media/style/' . $modx->config['manager_theme'] . '/images/';
 
 // Tree Menu Toolbar
-// Tree Menu Toolbar
 $_style['add_doc_tree']             = '<i class="fa fa-file fa-lg"></i>';
 $_style['add_weblink_tree']         = '<i class="fa fa-link fa-lg"></i>';
 $_style['collapse_tree']            = '<i class="fa fa-arrow-circle-up fa-lg"></i>';
@@ -38,7 +37,7 @@ $_style['tree_page_home']           = $style_path.'fontaw/home.png';
 $_style['tree_page_404']            = $style_path.'fontaw/warning-sign.png';
 $_style['tree_page_hourglass']      = $style_path.'fontaw/time.png';
 $_style['tree_page_info']           = $style_path.'fontaw/info.png';
-$_style['tree_page_blank']          = $style_path.'tree/application.png';
+$_style['tree_page_blank']          = $style_path.'fontaw/file.png';
 $_style['tree_page_css']            = $style_path.'fontaw/file-text-alt.png';
 $_style['tree_page_html']           = $style_path.'fontaw/file-alt.png';
 $_style['tree_page_xml']            = $style_path.'fontaw/file-text-alt.png';
@@ -64,50 +63,50 @@ $_style['tree_page_excel_secure']   = $style_path.'tree/application_excel_secure
 
 
 // Icons
-$_style['icons_add']                = $style_path.'icons/save.png';
-$_style['icons_cal']                = $style_path.'icons/cal.gif';
-$_style['icons_cal_nodate']         = $style_path.'icons/cal_nodate.gif';
-$_style['icons_cancel']             = $style_path.'icons/stop.png';
-$_style['icons_close']              = $style_path.'icons/stop.png';
-$_style['icons_delete']             = $style_path.'icons/delete.png';
-$_style['icons_delete_document']    = $style_path.'icons/delete.png';
-$_style['icons_resource_overview']  = $style_path.'icons/information.png';
-$_style['icons_resource_duplicate'] = $style_path.'icons/page_white_copy.png';
-$_style['icons_edit_document']      = $style_path.'icons/save.png';
-$_style['icons_email']              = $style_path.'icons/email.png';
-$_style['icons_folder']             = $style_path.'icons/folder.gif';
-$_style['icons_home']               = $style_path.'icons/home.gif';
-$_style['icons_information']        = $style_path.'icons/information.png';
-$_style['icons_loading_doc_tree']   = $style_path.'icons/information.png'; // top bar
-$_style['icons_mail']               = $style_path.'icons/email.png'; // top bar
-$_style['icons_message_forward']    = $style_path.'icons/forward.gif';
-$_style['icons_message_reply']      = $style_path.'icons/reply.gif';
-$_style['icons_modules']            = $style_path.'icons/modules.gif';
-$_style['icons_move_document']      = $style_path.'icons/page_white_go.png';
-$_style['icons_new_document']       = $style_path.'icons/page_white_add.png';
-$_style['icons_new_weblink']        = $style_path.'icons/world_link.png';
-$_style['icons_preview_resource']   = $style_path.'icons/page_white_magnify.png';
-$_style['icons_publish_document']   = $style_path.'icons/clock_play.png';
-$_style['icons_refresh']            = $style_path.'icons/refresh.png';
-$_style['icons_save']               = $style_path.'icons/save.png';
-$_style['icons_set_parent']         = $style_path.'icons/layout_go.png';
-$_style['icons_table']              = $style_path.'icons/table.gif';
-$_style['icons_undelete_resource']  = $style_path.'icons/b092.gif';
-$_style['icons_unpublish_resource'] = $style_path.'icons/clock_stop.png';
-$_style['icons_user']               = $style_path.'icons/user.gif';
-$_style['icons_weblink']            = $style_path.'icons/world_link.png';
-$_style['icons_working']            = $style_path.'icons/exclamation.png'; // top bar
-$_style['icons_event1']             = $style_path.'icons/event1.png';
-$_style['icons_event2']             = $style_path.'icons/event2.png';
-$_style['icons_event3']             = $style_path.'icons/event3.png';
-$_style['icons_secured']            = $style_path.'icons/secured.gif';
+$_style['icons_add']                = $style_path.'fontaw/save.png';
+$_style['icons_cal']                = $style_path.'fontaw/calendar.png';
+$_style['icons_cal_nodate']         = $style_path.'fontaw/calendar-empty';
+$_style['icons_cancel']             = $style_path.'fontaw/remove.png';
+$_style['icons_close']              = $style_path.'fontaw/stop.png';
+$_style['icons_delete']             = $style_path.'fontaw/trash.png';
+$_style['icons_delete_document']    = $style_path.'fontaw/trash.png';
+$_style['icons_resource_overview']  = $style_path.'fontaw/info.png';
+$_style['icons_resource_duplicate'] = $style_path.'fontaw/copy.png';
+$_style['icons_edit_document']      = $style_path.'fontaw/edit.png';
+$_style['icons_email']              = $style_path.'fontaw/envelope-alt.png';
+$_style['icons_folder']             = $style_path.'fontaw/folder-close-alt.png';
+$_style['icons_home']               = $style_path.'fontaw/home.png';
+$_style['icons_information']        = $style_path.'fontaw/info-sign.png';
+$_style['icons_loading_doc_tree']   = $style_path.'fontaw/info-sign.png'; // top bar
+$_style['icons_mail']               = $style_path.'fontaw/envelope-alt.png'; // top bar
+$_style['icons_message_forward']    = $style_path.'fontaw/forward.png';
+$_style['icons_message_reply']      = $style_path.'fontaw/reply.png';
+$_style['icons_modules']            = $style_path.'flat/cogs3.png';
+$_style['icons_move_document']      = $style_path.'fontaw/move.png';
+$_style['icons_new_document']       = $style_path.'fontaw/file-alt.png';
+$_style['icons_new_weblink']        = $style_path.'fontaw/link.png';
+$_style['icons_preview_resource']   = $style_path.'fontaw/eye-open.png';
+$_style['icons_publish_document']   = $style_path.'fontaw/check.png';
+$_style['icons_refresh']            = $style_path.'fontaw/refresh.png';
+$_style['icons_save']               = $style_path.'fontaw/ok-circle.png';
+$_style['icons_set_parent']         = $style_path.'fontaw/folder-open-alt.png';
+$_style['icons_table']              = $style_path.'fontaw/table.png';
+$_style['icons_undelete_resource']  = $style_path.'fontaw/upload.png';
+$_style['icons_unpublish_resource'] = $style_path.'fontaw/check-empty.png';
+$_style['icons_user']               = $style_path.'fontaw/user.png';
+$_style['icons_weblink']            = $style_path.'fontaw/link.png';
+$_style['icons_working']            = $style_path.'fontaw/warning-sign.png'; // top bar
+$_style['icons_event1']             = $style_path.'fontaw/info-sign.png';
+$_style['icons_event2']             = $style_path.'fontaw/warning-sign.png';
+$_style['icons_event3']             = $style_path.'fontaw/remove-sign.png';
+$_style['icons_secured']            = $style_path.'fontaw/lock.png';
 
 // Tabs
-$_style['icons_tab_preview']        = $style_path.'icons/preview.png';
+$_style['icons_tab_preview']        = $style_path.'fontaw/eye-open.png';
 
 // Indicators
-$_style['icons_tooltip']            = $style_path.'icons/b02.gif';
-$_style['icons_tooltip_over']       = $style_path.'icons/b02_trans.gif';
+$_style['icons_tooltip']            = $style_path.'fontaw/question-sign.png';
+$_style['icons_tooltip_over']       = $style_path.'fontaw/question-sign.png';
 
 // Large Icons
 $_style['icons_backup_large']       = $style_path.'flat/download.png';
@@ -120,7 +119,7 @@ $_style['icons_webusers_large']     = $style_path.'flat/group.png';
 // Miscellaneous
 $_style['ajax_loader']              = '<p>'.$_lang['loading_page'].'</p><p><img src="'.$style_path.'misc/ajax-loader.gif" alt="Please wait" /></p>';
 $_style['tx']                       = $style_path.'misc/_tx_.gif';
-$_style['icons_right_arrow']        = $style_path.'icons/circlerightarrow.gif';
+$_style['icons_right_arrow']        = $style_path.'flat/arrow.png';
 $_style['fade']                     = $style_path.'misc/fade.gif';
 $_style['ed_save']                  = $style_path.'misc/ed_save.gif';
 
